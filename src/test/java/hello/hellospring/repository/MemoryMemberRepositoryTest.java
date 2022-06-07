6ce1b7c6584be93e45a230c123ace89fc51ffd76
geonho1943 <thsrjsgh02@naver.com>
2022-06-07 23:59:43 +0900

    add MemoryMemberRepositoryTest

---
+++ src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,21 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class MemoryMemberRepositoryTest {
+    MemberRepository repository = new MemoryMemberRepository();
+
+    @Test
+    public void save(){
+        Member member = new Member();
+        member.setName("string");
+
+        repository.save(member);
+        Member result = repository.findById(member.getId()).get();
+        //4:40 syso로 보는법
+        Assertions.assertEquals(member,result);
+        //Assertions.assertThat(member).isEq
+    }
+}
