3ea8fc9fe9e06ac5a1024654698eb4633a4e7505
geonho1943 <thsrjsgh02@naver.com>
2022-06-21 23:54:03 +0900

    스프링 빈즈, autowired 의존관계 학습

---
+++ src/main/java/HelloSpringApplication.java
@@ -0,0 +1,11 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringApplication {
+    //구현체
+    public static void main(String[] args) {
+        SpringApplication.run(hello.hellospring.HelloSpringApplication.class, args);
+    }
+
+}
